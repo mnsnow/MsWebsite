002eedd5139ad784381226d578ea71aab6e775a0
mnsnow <lixx2813@umn.edu>
2017-11-05 23:12:44 -0600

    db change from sqlite to mysql;modified superuser info

--- ms_pages/models.py
+++ ms_pages/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """ A topic about maplestory """
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=300)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User)
-    
+
     def __str__(self):
         """ represent topic with a string of it's name """
         return self.text
--- ms_site/settings.py
+++ ms_site/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'ms_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ms_website',
+        'USER': 'root',
+        'PASSWORD': '19930819aa'
     }
 }
 
@@ -127,7 +129,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/Users/wenchangli/ms_website/ms_pages/static/static_root'
 
 STATICFILES_DIRS = [
-    '/Users/wenchangli/ms_website/ms_pages/static/static_dirs',
+    os.path.join(BASE_DIR, "ms_pages/static")
+    #'/Users/wenchangli/ms_website/ms_pages/static/static_dirs',
 ]
 
 MEDIA_ROOT = '/Users/wenchangli/ms_website/ms_pages/static/media/'
