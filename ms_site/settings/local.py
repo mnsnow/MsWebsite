98816d9fc5200558b8144f08a9aedb3334c769c8
mnsnow <lixx2813@umn.edu>
2017-11-08 02:02:02 -0600

    test

--- ms_site/settings/local.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ms_website',
-        'USER': 'root',
-        'PASSWORD': '19930819aa',#os.environ.get('MYSQL_PASS', 'fallback'),
-    }
-}
-#test111
