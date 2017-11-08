d2c8e13b159956a86798929582d9318354eec0d7
mnsnow <lixx2813@umn.edu>
2017-11-08 02:17:32 -0600

    fix heroku setting

--- ms_site/settings/base.py
+++ ms_site/settings/base.py
@@ -164,7 +164,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     DEBUG = False
 
     # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
